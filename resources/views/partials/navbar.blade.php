07d506e8d4498220d2ff87f772ca21c0a0cc07f4
Vitor Correia <vitorcorreia.git@gmail.com>
2026-04-19 08:38:29 -0300

    feat: improve navbar with auth and stock movements navigation

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -17,7 +17,7 @@
         </button>
 
         <div class="collapse navbar-collapse" id="mainNavbar">
-            <ul class="navbar-nav ms-auto">
+            <ul class="navbar-nav ms-auto align-items-lg-center">
                 <li class="nav-item">
                     <a
                         class="nav-link {{ request()->is('/') ? 'active fw-semibold' : '' }}"
@@ -44,14 +44,42 @@
                         Produtos
                     </a>
                 </li>
+
                 <li class="nav-item">
                     <a
                         class="nav-link {{ request()->routeIs('stock-movements.*') ? 'active fw-semibold' : '' }}"
-                        href="{{ route('stock-movements.create') }}"
+                        href="{{ route('stock-movements.index') }}"
                     >
-                       Movimentações
+                        Movimentações
                     </a>
                 </li>
+
+                @auth
+                    <li class="nav-item">
+                        <span class="nav-link text-white-50">
+                            {{ auth()->user()->name }}
+                        </span>
+                    </li>
+
+                    <li class="nav-item">
+                        <form action="{{ route('logout') }}" method="POST" class="ms-lg-2">
+                            @csrf
+
+                            <button type="submit" class="btn btn-sm btn-light">
+                                Sair
+                            </button>
+                        </form>
+                    </li>
+                @else
+                    <li class="nav-item">
+                        <a
+                            class="nav-link {{ request()->routeIs('login') ? 'active fw-semibold' : '' }}"
+                            href="{{ route('login') }}"
+                        >
+                            Login
+                        </a>
+                    </li>
+                @endauth
             </ul>
         </div>
     </div>
