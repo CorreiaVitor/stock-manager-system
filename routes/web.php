04c49d7aa796baba1814c30aff189947af1eb9dd
Vitor Correia <vitorcorreia.git@gmail.com>
2026-04-19 08:40:36 -0300

    feat: add login and logout routes

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,14 @@
 <?php
 
+use App\Http\Controllers\Auth\AuthenticatedSessionController;
 use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\StockMovementController;
 use Illuminate\Support\Facades\Route;
+
+Route::get('/login', [AuthenticatedSessionController::class, 'create'])->name('login');
+Route::post('/login', [AuthenticatedSessionController::class, 'store'])->name('login.store');
+Route::post('/logout', [AuthenticatedSessionController::class, 'destroy'])->name('logout');
 
 Route::view('/', 'welcome');
 Route::get('/categorias', [CategoryController::class, 'index'])->name('categories.index');
