86a206eb74f6c896bd28551df29fc60cc8358883
Vitor Correia <vitorcorreia.git@gmail.com>
2026-04-19 21:17:53 -0300

    feat: show footer only for authenticated users

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,4 +1,5 @@
- <div class="container">
+@auth
+     <div class="container">
         <footer class="py-3 my-4">
             <ul class="nav justify-content-center border-bottom pb-3 mb-3">
                 <li class="nav-item">
@@ -20,3 +21,4 @@
             <p class="text-center text-body-secondary">&copy; 2025 Company, Inc</p>
         </footer>
     </div>
+@endauth
