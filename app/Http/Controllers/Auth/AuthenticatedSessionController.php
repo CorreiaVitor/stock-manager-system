f0c63362e89f680ca13eb9964b006549aa5f5610
Vitor Correia <vitorcorreia.git@gmail.com>
2026-04-19 21:36:44 -0300

    feat: implement login and logout actions

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -3,7 +3,12 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Requests\Auth;
+use App\Http\Requests\Auth\LoginResquest;
+use App\Models\User;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\View\View;
 
 class AuthenticatedSessionController extends Controller
@@ -11,5 +16,26 @@ class AuthenticatedSessionController extends Controller
     public function create(): view
     {
         return view('auth.login');
+    }
+
+    public function store(LoginResquest $request) : RedirectResponse
+    {
+
+        $request->authenticate();
+
+        $request->session()->regenerate();
+
+        return redirect()->intended(route('stock-movements.index'));
+    }
+
+    public function destroy(Request $request)
+    {
+        Auth::logout();
+
+        $request->session()->invalidate();
+
+        $request->session()->regenerateToken();
+
+        return to_route('login');
     }
 }
